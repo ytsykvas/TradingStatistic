4781048bec77e67eb8e9576f8deb306788436961
tsykvas <tsykvasyurii@gmail.com>
2022-10-17 20:58:04 +0300

    Finished implementing Newscatcher API

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -10,7 +10,7 @@ class NewsController < ApplicationController
 
   def search
     host = {content_type: :json, accept: :json, "X-RapidAPI-Key": ENV["API_KEY"]}
-    response =  RestClient.get("https://newscatcher.p.rapidapi.com/v1/search_enterprise?q=#{params[:ticker]}&lang=en&sort_by=relevancy&page=1&media=True", host)
+    response =  RestClient.get("https://newscatcher.p.rapidapi.com/v1/search_enterprise?q=#{params[:ticker]}&lang=en&sort_by=date&page=1&media=True", host) #news sorted with date
     news = JSON.parse(response, symbolize_names: true)
     @news = news[:articles]
   end
